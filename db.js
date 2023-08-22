4d6ae3828febdb4c3cadba866b6be861b669ddc1
sp-santosh <santosh.poudel054@gmail.com>
2023-08-22 22:27:59 +0545

    Configuration to railway

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const { Pool } = require("pg");
 
 const dbPool = new Pool({
-  user: "admin",
-  password: "admin",
-  host: "localhost",
-  port: 5432,
-  database: "todo",
+  user: "postgres",
+  password: "QsYpJCEzATVLCgFYr0hV",
+  host: "containers-us-west-33.railway.app",
+  port: 6290,
+  database: "railway",
+
+  //RailWay//
+
+  // user: "admin",
+  // password: "admin",
+  // host: "localhost",
+  // port: 5432,
+  // database: "todo",
 });
 
 module.exports = dbPool;
